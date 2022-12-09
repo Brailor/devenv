12634daa670861aab5810e4cddfe85f4593ac650
Viktor Ohád <viktor.ohad@protonmail.com>
2022-12-09 23:32:47 +0100

    trying things

--- nix_installer.sh
+++ nix_installer.sh
@@ -4,7 +4,9 @@ set -e
 
 echo "Setting up nix..."
 export NIX_INSTALLER_YES=true
-curl -L https://nixos.org/nix/install --daemon
+curl -L https://nixos.org/nix/install -o nix_installer
+chmod +x nix_installer
+source nix_installer --daemon
 
 echo "Done installing nix."
 nix-shell -p nix-info --run "nix-info -m"
